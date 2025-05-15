2cdf3c09dead783f3a3e7f693f936d350213da2b
karol <karol.kruszynski0@gmail.com>
2025-05-15 15:29:38 +0200

    Data Cleaining

--- scripts/main.py
+++ scripts/main.py
@@ -32,3 +32,26 @@ print(f"Percentage of missing data {(df.isnull().sum() / len(df) * 100).sort_val
 # DATA CLEANING
 # Crucial data: Age, Weight, Results
 # If row didn't have crucial data then it will be dropped
+
+df = df.dropna(subset=["Age", "BodyweightKg", "TotalKg"])
+print(f"Number of rows after cleaning: {df.shape[0]}")
+clean_data_rows = df.shape[0]
+print(f"Percentage change: {round((raw_data_rows - clean_data_rows) / raw_data_rows * 100, 2)}%")
+
+# Drop columns that will not be used in futher analysis
+df = df.drop(["Best3DeadliftKg", "State", "MeetState", "MeetTown", "MeetName", "Sanctioned"], axis=1)
+# Round the age 0.5 to upper
+df["Age"] = np.ceil(df["Age"]).astype(int)
+print(df["Age"].dtypes)
+
+# Check unique values to get errors in data
+print(f"Unique values: {df.nunique()}")
+print(f"Unique countries: {df['Country'].nunique()}")
+print(f"Unique sex types: {df['Sex'].unique()}")
+
+# Fix 'Mx' Sex
+df['Sex'] = df['Sex'].replace('Mx', 'M')
+print(f"Unique sex types: {df['Sex'].unique()}")
+
+# Save cleaned data
+df.to_csv("cleaned_openpowerlifting.csv", index=False)
