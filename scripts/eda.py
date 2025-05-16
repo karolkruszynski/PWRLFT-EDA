93f859325d752b3e0af5b479153cf7d41ff1bc16
karol <karol.kruszynski0@gmail.com>
2025-05-16 19:02:02 +0200

    simple hist plot for age disctribution

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("../data/sample_data.csv", low_memory=False)
+sns.histplot(df['Age'].dropna(), bins=50, kde=True)
+plt.title("Age distribution of lifters")
+plt.xlabel("Age")
+plt.ylabel("Number of lifters")
+plt.show()
