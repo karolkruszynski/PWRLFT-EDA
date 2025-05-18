a092314fac363ff98298060c8f14cd5c96c097ba
karol <karol.kruszynski0@gmail.com>
2025-05-18 18:46:38 +0200

    Visualization of strength distributions for men and women by weight category

--- scripts/eda.py
+++ scripts/eda.py
@@ -34,3 +34,32 @@ g.set_titles("{col_name}")
 plt.suptitle("Comparison of Wilks / Dots / Glossbrenner indices - Separate for gender.", y=1.05)
 plt.tight_layout()
 plt.show()
+
+# Visualization of strength distributions for men and women by weight category
+#print(df['WeightClassKg'].unique())
+df_raw = df[df['Equipment'] == 'Raw']
+
+def parse_weight_class(val):
+    try:
+        return float(val.replace('+', '').replace('-','-'))
+    except:
+        return np.nan
+
+df_raw['WeightClassKg_clean'] = df_raw['WeightClassKg'].astype(str).apply(parse_weight_class)
+print(df_raw['WeightClassKg_clean'].unique())
+bins = [0, 59, 69, 79, 89, 99, 109, 120, 200]
+labels = ['≤59', '60–69', '70–79', '80–89', '90–99', '100–109', '110–120', '120+']
+
+df_raw['WeightCategory'] = pd.cut(df_raw['WeightClassKg_clean'], bins=bins, labels=labels, right=False)
+plt.figure(figsize=(14, 6))
+sns.barplot(data=df_raw,
+            x='WeightCategory',
+            y='TotalKg',
+            hue='Sex',
+            palette='pastel')
+plt.title("Distribution of TotalKg results by weight category and gender")
+plt.xlabel("Weight category (kg)")
+plt.ylabel("TotalKg score")
+plt.legend(title="Sex")
+plt.tight_layout()
+plt.show()
